f453212a37d99a10adb5e806ae9f39913089d65d
giselacruz <62399884+giselacruz@users.noreply.github.com>
2020-06-21 14:03:20 -0300

    add class generic dataAccessService

--- src/com/company/DataAccess/Services/DataAccessService.java
+++ src/com/company/DataAccess/Services/DataAccessService.java
@@ -19,22 +19,83 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 
-public class DataAccessService {
+public class DataAccessService <T> {
 
     private static ObjectMapper mapper = new ObjectMapper();
-    private File fileUser = new File("User.json");
-    private File filePlane = new File("Plane.json");
-    private File FileUserflight = new File("UserFlight.json"); //change dejalo pero quizas no lo usemos
-    private File FileCompanyflight = new File("CompanyFlight.json"); //change dejalo pero quizas no lo usemos
+    private String fileUser;
+    private String filePlane;
     private File FileCompany = new File("Company.json");
 
+    private File FileUserflight = new File("UserFlight.json"); //change dejalo pero quizas no lo usemos
+    private File FileCompanyflight = new File("CompanyFlight.json"); //change dejalo pero quizas no lo usemos
 
-    public DataAccessService(){};
+    //constructor
+    public DataAccessService() {
+    }
+
+    ;
+
+    // getnamefile
+    public String getNameFileUser() {
+        return "User.json";
+    }
+
+    public String getNameFilePlane() {
+        return "Plane.json";
+    }
+
+    // guarda lista de cualquier tipo en el json pasado por parametro
+    private void writeListJSON(List<T> type, String json) throws JsonGenerationException, JsonMappingException, IOException {
+        mapper.writeValue(new File(json), type);
+    }
+
+    //lee una lista de cualquier tipo en el json que le pases por parametro
+    private List<T> readListFileJSON(String json) throws JsonParseException, JsonMappingException, IOException {
+        List<T> type = mapper.readValue(new File(json), List.class);
+        System.out.println(type.toString());
+        return type;
+    }
+
+    //lee una hashSet de cualquier tipo de un json pasado por parametro
+    private HashSet<T> readHasSet(String json) throws JsonParseException, JsonMappingException, IOException {
+        HashSet<T> type = mapper.readValue(new File(json), HashSet.class);
+        System.out.println(type.toString());
+        return type;
+    }
+
+    //escribe un hashset en un json de cualquier tipo
+    private void writeHashSet(HashSet<T> type, String json) throws JsonGenerationException, JsonMappingException, IOException {
+        mapper.writeValue(new File(json), type);
+    }
     //------------------------- Change
     //trae la lista de usuarios, compara por documento los usuarios en la lista,
     //si encuenta coincidencia devuelve el usuariocon sus datos, sino devuelve el objeto con el documento asignado
     //para crear el nuevousario
+
+    //estos dos metodos ya  los adapte a la clase dataAccessService generica
+
     public User searchUserInData(int document) throws IOException {
+
+        DataAccessService<User> userDataAccessService = new DataAccessService<>();
+        List<User> userList = userDataAccessService.readListFileJSON(getNameFileUser());
+        User newUser = new User(document);
+        newUser = checkExistence(userList, newUser);
+        return newUser;
+    }
+
+    //Compara los usuariios por documentos
+    private User checkExistence(List<User> userList, User newUser) {
+        for (User savedUser : userList) {
+            if (newUser.getUserDocument() == savedUser.getUserDocument()) {
+                newUser = savedUser;
+            }
+        }
+        return newUser;
+    }
+
+    // esto es lo que habia antes::
+
+   /* public User searchUserInData(int document) throws IOException {
         List<User> userList =readListUserFile();
         User newUser = new User(document);
         newUser=checkExistence(userList,newUser);
@@ -49,24 +110,30 @@ public class DataAccessService {
             }
         }
         return newUser;
-    }
-
-    //Con este metodo traigo un objeto de tipo Flight, si es UserFlight, llama al metodo de escritura de usuarios
-    //si es tipo CompanyFlight, llama al metodo de escritura de datos en compañia
-    public void dataSaveFlight(User user, Flight flight) throws IOException {
-        if (flight instanceof UserFlight)
-        {
-            addNewFlightToUser(user, (UserFlight)flight);
-        }
-        if (flight instanceof CompanyFlight)
-        {
-            writeCompanyFile(flight);
-        }
-    }
+    }*/
 
     //metodo de datos de compañia, recibe un tipo OBJECT se busca en la base de datos la compañia
     //luego se ve q tipo de objeto es y se agrega a la lista correspondiente de la compañia
-    private void writeCompanyFile(Object obj) throws JsonGenerationException, JsonMappingException, IOException{
+    //este metodo ya lo adapte a la clase para llamarla se inicialisa un DataAccessService <T> pasasandole un tipo y lugo llamas escribirCompanyF
+
+    private void writeCompanyFile(T obj) throws JsonGenerationException, JsonMappingException, IOException {
+        Company company = readCompanyFile();
+        if (obj instanceof Flight) {
+            company.addCompanyFlights((CompanyFlight) obj);
+        }
+        if (obj instanceof User) {
+            company.addUsers((User) obj);
+        }
+        if (obj instanceof City) {
+            company.addCitys((City) obj);
+        }
+        if (obj instanceof Plane) {
+            company.addPlanes((Plane) obj);
+        }
+        mapper.writeValue(FileCompany, company);
+    }
+
+    /* private void writeCompanyFile(Object obj) throws JsonGenerationException, JsonMappingException, IOException{
         Company company = readCompanyFile();
         if(obj instanceof CompanyFlight){
             company.addCompanyFlights((CompanyFlight) obj);
@@ -81,7 +148,7 @@ public class DataAccessService {
             company.addPlanes((Plane)obj);
         }
         mapper.writeValue(FileCompany,company);
-    }
+    }*/
 
     private Company readCompanyFile() throws JsonParseException, JsonMappingException, IOException {
         Company company = mapper.readValue(FileCompany, Company.class);
@@ -90,16 +157,39 @@ public class DataAccessService {
     }
 
     //trae al nuevo usuario, lo agrega a la lista de usuarios de la compañia, lo agrega en el archivo de usuarios
+    // adaptada a la clase
     public void saveNewUser(User newUser) throws IOException {
+        writeCompanyFile(newUser);
+        DataAccessService<User> userDataAccessService = new DataAccessService<User>();
+        List<User> userList = userDataAccessService.readListFileJSON(this.getNameFileUser());
+        userList.add(newUser);
+        userDataAccessService.writeListJSON(userList, this.getNameFileUser());
+    }
+
+    // lo que habia ::
+    /*public void saveNewUser(User newUser) throws IOException {
         writeCompanyFile(newUser);
 
         List<User> userList = readListUserFile();
         userList.add(newUser);
         writeListUserFile(userList);
-    }
+    }*/
 
     //metodo para agregar un vuelo a la lista de vuelos de un usuario
-    public void addNewFlightToUser(User user, UserFlight flight) throws IOException {
+    //adaptada a la clase
+     public void addNewFlightToUser(User user, UserFlight flight) throws IOException {
+         DataAccessService<User> userDataAccessService = new DataAccessService<User>();
+        List<User> usersList = userDataAccessService.readListFileJSON(this.getNameFileUser());
+        for (User userToAdd : usersList) {
+            if(userToAdd.getUserDocument()==user.getUserDocument())
+            {
+                userToAdd.addFlight(flight);
+            }
+        }
+        userDataAccessService.writeListJSON(usersList,this.getNameFileUser());
+    }
+   // lo que habia
+    /*public void addNewFlightToUser(User user, UserFlight flight) throws IOException {
         List<User> usersList = readListUserFile();
         for (User userToAdd : usersList) {
             if(userToAdd.getUserDocument()==user.getUserDocument())
@@ -108,20 +198,7 @@ public class DataAccessService {
             }
         }
         writeListUserFile(usersList);
-    }
-
-   // guarda la lista de usuarios
-    private void writeListUserFile(List<User> userList ) throws JsonGenerationException, JsonMappingException, IOException {
-        mapper.writeValue(fileUser,userList);
-    }
-
-    //lee lista en json y la devuelve cuando la devuelve se puede buscar un usuario en esa lista QUE devuelve
-    private List<User> readListUserFile() throws JsonParseException, JsonMappingException, IOException {
-        List<User> users =mapper.readValue(fileUser, List.class);
-        System.out.println(users.toString());
-        return users;
-    }
-
+    }*/
     public CompanyFlight searchFlightInData(UserFlight userFlight) throws IOException {
         Company company = readCompanyFile();
         CompanyFlight newFlight = checkFlightExistence(company.getCompanyFlightsList(),userFlight);
@@ -141,4 +218,4 @@ public class DataAccessService {
         return newFlight;
     }
 
-}
+} 
